0c6ae59a837e6822d1fbd17b67478cde00dda37c
alishazin <78612608+alishazin@users.noreply.github.com>
2025-03-19 16:46:19 +0530

    2 and 3 done

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.lab2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void openNextActivity(View v) {
+
+        Intent implicit = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(implicit);
+
+    }
+
+    public void openWebsite(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.google.com"));
+        startActivity(intent);
+
+    }
+
+}
